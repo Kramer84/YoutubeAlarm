3951010bde9d09998cf71c0f0163297b26f632c6
motherloadSig.ksimady <kristof.attila.private@gmail.com>
2025-11-25 14:04:28 +0100

    chore: sync 2025-11-25 14:04:28+0100 motherloadSig.ksimady@motherload — some udpates

--- scripts/create_python_env.sh
+++ scripts/create_python_env.sh
@@ -4,19 +4,19 @@
 ENV_NAME="youtubeAlarm"
 
 # Create the conda environment and install all the necessary packages in one step
-conda create --name $ENV_NAME python=3.8 \
-              conda-forge::yt-dlp \
-              conda-forge::requests \
-              conda-forge::ffmpeg \
-              conda-forge::psutil \
-              conda-forge::aioconsole \
-              conda-forge::mutagen -y
+conda create --name $ENV_NAME python=3.10 \
+    conda-forge::ffmpeg \
+    conda-forge::psutil \
+    conda-forge::aioconsole \
+    conda-forge::mutagen \
+    conda-forge::requests -y
 
 # Activate the environment
-source activate $ENV_NAME
+conda activate $ENV_NAME
 
 # Install additional libraries via pip if necessary
 pip install python-vlc
+pip install -U yt-dlp  # -U ensures you get the latest version
 
 # Deactivate the environment
 conda deactivate
